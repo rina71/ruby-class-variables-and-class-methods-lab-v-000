08a75fb2c15c1b9fdcfe984276bb1cfff278f400
Yurina Shin <yurinas@gmail.com>
2017-12-26 17:00:23 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = [ ]
+  @@artists = [ ]
+  @@artist_count = { }
+  @@genre_count = { }
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+  def self.genres
+
+    @@genres.uniq
+  end
+  def self.artists
+
+    @@artists.uniq
+  end
+  def self.genre_count
+    # @@genres.
+  end
+  def self.artist_count
+    # @@artist_count.count
+  end
+end
